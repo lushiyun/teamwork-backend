c62ed0a4b526b8d75cb897237f9a3c9e98a81e2c
github username <lushiyun@outlook.com>
2020-09-13 01:54:00 -0400

    Add basic user and team controllers

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,41 @@
 class TeamsController < ApplicationController
+  before_action :set_user, only: [:show, :update]
+
+  def index
+    teams = Team.all
+    options = { include: :users }
+    render json: TeamSerializer.new(teams, options).serialized_json
+  end
+
+  def show
+    options = { include: :users }
+    render json: TeamSerializer.new([@team], options).serialized_json
+  end
+
+  def create
+    team = Team.new(team_params)
+    if team.save
+      render json: TeamSerializer.new([team]).serialized_json
+    else
+      render json: { error: 'could not be created' }
+    end
+  end
+
+  def update
+    if @team.update(team_params)
+      render json: TeamSerializer.new([team]).serialized_json
+    else
+      render json: { error: 'could not be updated' }
+    end
+  end
+
+  private
+
+  def set_team
+    @team = Team.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :picture_url)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: UserSerializer.new(users).serialized_json
+  end
+
+  def show
+    user = User.find_by_id(params[:id])
+    render json: UserSerializer.new([user]).serialized_json
+  end
+
+  def create
+    if user = User.find_by(email: params[:email])
+      user.update(user_params)
+    else
+      user = User.create(user_params)
+    end
+    render json: UserSerializer.new([user]).serialized_json
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :picture_url)
+  end
 end
