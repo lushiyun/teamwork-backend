b0ddb92d320b730f964fe8ecbbb14845eb9291ba
github username <lushiyun@outlook.com>
2020-10-11 12:37:11 -0400

    Add redis configuration for websocket in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,9 +32,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.mount_path = '/cable'
+  config.action_cable.url = 'wss://teamwork-server.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://teamwork-client.netlify.app', /https:\/\/teamwork-client.netlify.app.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
