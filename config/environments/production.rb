346dcf3a4f642defd4b167a338379c14e11b8ef3
github username <lushiyun@outlook.com>
2020-10-12 17:24:55 -0400

    Revise websocket backend configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,8 +33,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   config.action_cable.mount_path = '/cable'
-  config.action_cable.url = 'wss://teamwork-server.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://teamwork-client.netlify.app', /https:\/\/teamwork-client.netlify.app.*/ ]
+  config.web_socket_server_url = 'wss://teamwork-server.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://teamwork-client.netlify.app', 'https://teamwork-client.netlify.app', /https:\/\/teamwork-client.netlify.app.*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
